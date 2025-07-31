d55020ebc55ddc45769507b72ac906d6e61c1efd
Filip Kunc <kunc.filip@gmail.com>
2025-07-31 17:19:22 +0200

    Fix `cd`

--- hooks_bench/bench_hooks.sh
+++ hooks_bench/bench_hooks.sh
@@ -3,4 +3,4 @@ yarn run compile:bench
 hyperfine --warmup 3 --export-markdown=benchTable.md \
 'node runtime.mjs' \
 'node --import ./sync-hooks-babel.mjs runtime.mjs' \
-'node --import ./sync-hooks-oxc.mjs runtime.mjs' \
+'node --import ./sync-hooks-oxc.mjs runtime.mjs'
