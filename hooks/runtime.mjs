39da9e9817e66139811a13eb1f45a7e7566336bd
Filip Kunc <kunc.filip@gmail.com>
2025-08-07 20:20:57 +0200

    Basic memory usage logging

--- hooks/runtime.mjs
+++ hooks/runtime.mjs
@@ -1,5 +1,11 @@
 import './wrap.mjs';
-import { handler } from './handler.mjs';
+
+const rssBefore = process.memoryUsage.rss();
+const handlerModule = await import('./handler.mjs');
+const handler = handlerModule.handler;
+const rssAfter = process.memoryUsage.rss();
+
+const maxRss = Math.max(rssBefore, rssAfter);
 
 const res = await handler({ foo: 'bar' }, {});
-console.log(res);
+console.log(`${res}\nmaxRss: ${maxRss / (1024 * 1024) } MB`);
